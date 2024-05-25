999b4be1371cd689a38297ea2a8061005c067fa5
Grzegorz Latosinski <glatosinski@antmicro.com>
2024-05-25 15:57:20 +0200

    delegate-example: clone-tensorflow.sh: Modified script to clone TensorFlow v2.15.0
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- delegate-example/clone-tensorflow.sh
+++ delegate-example/clone-tensorflow.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-git clone --recursive https://github.com/tensorflow/tensorflow.git -b v2.7.0 tensorflow
-cd tensorflow/
-git am ../0001-tensorflow-lite-Added-limits-to-simple_memory_arena_.patch
-cd -
+git clone --recursive https://github.com/tensorflow/tensorflow.git -b v2.15.0 tensorflow
